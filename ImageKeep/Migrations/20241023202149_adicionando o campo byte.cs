bee07bdddef2610bc3fad53dd914dd47442b285f
ArthurEmanuel <Arthurresende2004@gmail.com>
2024-10-23 17:22:42 -0300

    Adicionando o campo de bytes

---
+++ ImageKeep/Migrations/20241023202149_adicionando o campo byte.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ImageKeep.Migrations
+{
+    /// <inheritdoc />
+    public partial class adicionandoocampobyte : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Imagem",
+                table: "Fotos",
+                type: "BLOB",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Imagem",
+                table: "Fotos");
+        }
+    }
+}
